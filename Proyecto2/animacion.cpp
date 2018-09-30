a4eda084b2e4042a3ed8b01d717407ba7783b8f7
BlackPanter115 <miguel.rojas115@hotmail.com>
2018-09-29 20:27:23 -0500

    Makefile Editado

--- Proyecto2/animacion.cpp
+++ Proyecto2/animacion.cpp
@@ -12,20 +12,20 @@ int main(int argc, char const *argv[])
 	if (argc < 2) {
 		cout << endl << "Argumentos insuficientes, por favor escriba el comando de la siguiente manera: " << endl;
 		cout << "\t./animacion [no. de Asteriodes]" << endl;
+		cout << "\tmake n=[no de asteroides]" << endl << endl;
 		return -1;
 	}
-
 	srand(time(NULL));				//Generador de numeros random
 
 	/*DECLARACION DE VARIABLES*/
-	int n = atoi(argv[1]); 			//Numero de asteroides para dibujar
-	int vertices;					//Numero de vertices para cada asteoride
+	int n = atoi(argv[1]); 					//Numero de asteroides para dibujar
+	int vertices = 0;				//Numero de vertices para cada asteoride
 	vector<Asteroide> asteroides;	//Vector de tipo Asteroide
 
 
 	/*GENERACION DE NUMERO DE VERTICES PARA CADA ASTEROIDE*/
-	for(int i = 0; i<n; i++){
-		vertices = rand() % 12 + 5;
+	for(int i = 0; i<=n; i++){
+		vertices = rand() % 8 + 3;
 		Asteroide	p(vertices);
 		asteroides.push_back(p);
 	}
@@ -34,23 +34,41 @@ int main(int argc, char const *argv[])
 	gfx_open(800, 600, "Proyecto 2 - Equipo 3");
 	gfx_color(0,200,100);
 
+
 	//Declaracion de un iterador para el vector asteorides
-	vector<Asteroide>::iterator ast = asteroides.begin();
+	vector<Asteroide>::iterator pol = asteroides.begin();
+
+	/*GENERACION DEL FACTOR DE ESCALA Y DE DESPLAZAMIENTO PARA CADA ASTEROIDE*/
+	/*while(cont < n){
+		//int factor = rand() % 100 + 10;
+		//int desplazamiento = rand() % 600 + 100;
+		//pol->aplicarFactorYDesplazamiento(desplazamiento);
+		pol++;
+		cont++;
+	}*/
+
+	//Calculo del angulo de rotacion
 
 	/**DIBUJAR TODOS LOS ASTEROIDES CON ROTACION Y TRASLACION*/
 	for(int i = 0 ; i < 1000 ; i++) {
 		gfx_clear();
-		ast = asteroides.begin();
+		pol = asteroides.begin();
 
-		for (ast = asteroides.begin(); ast != asteroides.end(); ast++){
-			ast->dibujar();
+		for (pol = asteroides.begin(); pol != asteroides.end(); pol++) {
+			pol->dibujar();
+			//pol->traslacion();
 		}
 
 		for(int j = 0; j<=n; j++){
+			
+			//Traslacion
 			asteroides[j].traslacion();
 		}
-			
-		usleep(41666);
+
+		//for(int t = 0; t < 100; t++){
+			usleep(41666);
+		//}
+		
 	}
 
 	return 0;
